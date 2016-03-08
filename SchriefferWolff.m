7734246575850fbf3a07ce0311320337e6d3601a
adamfrees <ajfrees@gmail.com>
2016-03-08 11:54:25 -0600

    changed MatrixExp to Taylor Series, more efficient on larger matrices

--- SchriefferWolff.m
+++ SchriefferWolff.m
@@ -6,7 +6,7 @@ computePerturbativeTranformation::usage =
 	"computePerturbativeTranformation[ inputH0, inputV, inputOrder] takes in a base Hamiltonian inputH0, a perturbation inputV, and calculates the SW Transformation to the inputOrder-th order."
 
 Begin["Private`"]
-computePerturbativeTranformation[inputH0_,inputV_,inputOrder_]:=Module[{H0=inputH0,V=inputV,order=inputOrder,HTot,dim,Hextra,Comm,Sadd,Hextra2,n,m,i,S2,H1,H2,Heff,Stot,A},(
+computePerturbativeTranformation[inputH0_,inputV_,inputOrder_]:=Module[{H0=inputH0,V=inputV,order=inputOrder,HTot,dim,Hextra,Comm,Sadd,Hextra2,n,m,i,S2,H1,H2,Heff,Stot,A,toReturn},(
 HTot = H0+V;
 dim=Dimensions[H0][[1]];
 Comm[A_,B_]:=A.B-B.A;
@@ -17,7 +17,9 @@ Hextra = If[i==1,V,If[i==2,\[Minus]Comm[Stot,Comm[Stot,H0]]/2+I Comm[Stot,V],Thr
 Sadd=Simplify[Table [If[ n==m,0,-((I Hextra[[n,m]])/(H0[[n]][[n]]\[Minus]H0[[m]][[m]]))],{n,1,dim},{m,1,dim}]];
 Stot = Stot + (A^i)*Sadd;
 ];
-Normal[Series[MatrixExp[-I*Stot],{A,0,order}]]/.{A->1}
+toReturn = IdentityMatrix[dim];
+Do[toReturn = toReturn+MatrixPower[-I*Stot,i]/i!,{i,1,order,1}];
+Normal[Series[toReturn,{A,0,order}]]/.{A->1}
 )]
 End[]
 EndPackage[]
